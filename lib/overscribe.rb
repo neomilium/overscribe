b15872137a77667929ee111179055e1ed4d3dd17
Romuald Conty <neomilium@gmail.com>
2021-01-01 21:46:36 +0100

    Implement the basics

--- lib/overscribe.rb
+++ lib/overscribe.rb
@@ -1,9 +1,75 @@
 require 'overscribe/version'
 
+require 'yaml'
+require 'English'
+
+require 'byebug'
+
 module Overscribe
   class Error < StandardError; end
 
-  def fetch
-    puts 'hello'
+  def self.fetch(cli_options)
+    collections = config['collections']
+
+    collections.select! do |collection, options|
+      if cli_options['pattern'].nil?
+        # Remove manually synced
+        !options['manual']
+      else
+        # Select by pattern
+        collection.start_with? cli_options['pattern']
+      end
+    end
+
+    collections.each do |collection, options|
+      directory = config['directories'][options['mediatype']]
+      directory = File.expand_path directory
+      directory = File.join directory, collection
+      puts directory
+      FileUtils.mkdir_p directory
+      FileUtils.chdir(directory) do
+        YoutubeDL.download url: options['url'], mediatype: options['mediatype'].to_sym,
+                           options: cli_options.merge(options)
+      end
+    end
+  end
+
+  def self.config
+    filename = File.expand_path '~/.overscribe.yaml'
+    YAML.safe_load(File.read(filename))
+  end
+end
+
+module Overscribe
+  module YoutubeDL
+    def self.download(url:, mediatype:, options:)
+      args = case mediatype
+             when :audio
+               %w[--format bestaudio --extract-audio]
+             when :video
+               %w[--format bestvideo+bestaudio]
+             else
+               raise NotImplementedError
+             end
+      args += %W[--max-downloads #{options['limit']}] unless (options['limit']).zero?
+      args += %W[--output #{options['filename_pattern']}] unless options['filename_pattern'].nil?
+
+      command = %w[youtube-dl --add-metadata --yes-playlist]
+      command += %w[--write-info-json]
+      command += %w[--simulate] if options['noop'] == true
+      command += %w[--download-archive .youtube-dl.archive]
+      command += %w[--ignore-errors]
+      command += %w[--no-call-home]
+      command += args
+      command += [url]
+      run command
+    end
+
+    def self.run(command)
+      puts "Running '#{command}'"
+      system(*command)
+      status = $CHILD_STATUS
+      puts status
+    end
   end
 end
--- lib/overscribe/cli.rb
+++ lib/overscribe/cli.rb
@@ -4,9 +4,22 @@ require 'thor'
 
 module Overscribe
   class CLI < Thor
-    desc 'fetch', 'Fetch specified in config file'
-    def fetch
-      ModuleSync.fetch
+    desc 'fetch-all', 'Fetch all collections'
+    option :limit,
+           desc: 'Limit downloads per collection',
+           type: :numeric,
+           default: 1
+    def fetch_all
+      Overscribe.fetch(options)
+    end
+
+    desc 'fetch PATTERN', 'Fetch collection that match pattern'
+    option :limit,
+           desc: 'Limit downloads per collection',
+           type: :numeric,
+           default: 1
+    def fetch(pattern)
+      Overscribe.fetch(options.merge(pattern: pattern))
     end
 
     def self.exit_on_failure?
