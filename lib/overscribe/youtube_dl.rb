b3b30061bcb9fdc87b5cd4341250ec130bfbb2ff
Romuald Conty <neomilium@gmail.com>
2021-01-14 16:43:34 +0100

    Move YoutubeDL module into dedicated file

---
+++ lib/overscribe/youtube_dl.rb
@@ -0,0 +1,29 @@
+module Overscribe
+  module YoutubeDL
+    MEDIATYPE_ARGS = {
+      audio: %w[--format bestaudio --extract-audio],
+      video: %w[--format bestvideo+bestaudio],
+    }.freeze
+
+    def self.download(url:, mediatype:, options:)
+      command = %w[youtube-dl --add-metadata --yes-playlist]
+      command += %w[--ignore-errors --no-call-home]
+      command += %w[--download-archive .youtube-dl.archive]
+
+      command += %w[--simulate] if options['noop'] == true
+      command += %W[--max-downloads #{options['limit']}] unless (options['limit']).zero?
+      command += %W[--output #{options['filename_pattern']}] unless options['filename_pattern'].nil?
+      command += MEDIATYPE_ARGS[mediatype]
+
+      command += [url]
+      run command
+    end
+
+    def self.run(command)
+      puts "Running '#{command}'"
+      system(*command)
+      status = $CHILD_STATUS
+      puts status
+    end
+  end
+end
